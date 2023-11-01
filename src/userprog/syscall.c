8707438a7e860eb489da239f5f5e08f967524e78
sseung45 <sseung45@postech.ac.kr>
2023-11-01 12:54:21 +0900

    syscall_handler frame 구현

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -3,6 +3,8 @@
 #include <syscall-nr.h>
 #include "threads/interrupt.h"
 #include "threads/thread.h"
+#include "threads/vaddr.h"
+#include "lib/user/syscall.h"
 
 static void syscall_handler (struct intr_frame *);
 
@@ -15,6 +17,39 @@ syscall_init (void)
 static void
 syscall_handler (struct intr_frame *f UNUSED) 
 {
-  printf ("system call!\n");
-  thread_exit ();
+  //printf ("system call!\n");
+  //thread_exit ();
+
+  int args[4];
+  int *esp = f->esp;
+  check_user_address(esp);
+
+  switch (*esp) {
+    case SYS_HALT: // 0 arguement
+      halt();
+      break;
+    case SYS_EXIT: // 1 arguement
+      get_argument(esp, args, 1);
+      exit(args[0]);
+      break;
+    case SYS_EXEC: // 1 arguement
+      get_argument(esp, args, 1);
+      f->eax = exec((const char *)(&args[0]));
+    case SYS_WAIT:
+      get_argument(esp, args, 1); // 1 arguement
+      f->eax = wait((pid_t *)(&args[0]));
+    case SYS_CREATE: // 2 arguements
+      get_argument(esp, args, 2);
+      f->eax = create((const char *)(&args[0]), (const char *)(&args[1]));
+    case SYS_REMOVE: // 1 arguement
+      get_argument(esp, args, 1);
+      f->eax = remove((const char *)(&args[0]));
+    case SYS_OPEN:
+    case SYS_FILESIZE:
+    case SYS_READ:
+    case SYS_WRITE:
+    case SYS_SEEK:
+    case SYS_TELL:
+    case SYS_CLOSE:
+  }
 }
