f084cb4e1c73db58ea8af445eca6772eae2031cc
sseung45 <sseung45@postech.ac.kr>
2023-11-01 18:37:09 +0900

    exec - process_execute 함수 수정

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -38,8 +38,14 @@ process_execute (const char *file_name)
     return TID_ERROR;
   strlcpy (fn_copy, file_name, PGSIZE);
 
+  char* save_ptr;
+  char* token = strtok_r(file_name, " ", &save_ptr);
+  if (filesys_open(token) == NULL) {
+    return -1;
+  }
+
   /* Create a new thread to execute FILE_NAME. */
-  tid = thread_create (file_name, PRI_DEFAULT, start_process, fn_copy);
+  tid = thread_create (token, PRI_DEFAULT, start_process, fn_copy);
   if (tid == TID_ERROR)
     palloc_free_page (fn_copy); 
   return tid;
