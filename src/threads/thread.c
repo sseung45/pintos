b24ca949399228a38f4fd78d4f365553a840e5aa
sseung45 <sseung45@postech.ac.kr>
2023-11-04 16:56:46 +0900

    괄호 수정

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -472,9 +472,9 @@ init_thread (struct thread *t, const char *name, int priority)
 
   // project 2
   t->parent = running_thread();
-  sema_init (&t->child_lock, 0);
-  sema_init (&t->load_lock, 0);
-  sema_init (&t->exit_lock, 0);
+  sema_init (&(t->child_lock), 0);
+  sema_init (&(t->load_lock), 0);
+  sema_init (&(t->exit_lock), 0);
   list_init (&(t->child_list));
   list_push_back (&(running_thread()->child_list), &(t->child_elem));
   for (int i = 0; i < 128; i++)
