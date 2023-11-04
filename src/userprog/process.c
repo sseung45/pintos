d341d7bfb3f81b3c74d0220d85fbb6f60ffe25c4
wnstodn123 <wnstodn123@postech.ac.kr>
2023-11-04 15:05:22 +0900

    file close 2번 되는 오류 수정

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -406,7 +406,6 @@ load (const char *file_name, void (**eip) (void), void **esp)
 
  done:
   /* We arrive here whether the load is successful or not. */
-  file_close (file);
   return success;
 }
 
