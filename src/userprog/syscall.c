446955968006d049bd209ce2a9a390f5a397b026
wnstodn123 <wnstodn123@postech.ac.kr>
2023-11-04 16:31:26 +0900

    hex_dump 사이즈 수정

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -44,7 +44,7 @@ syscall_handler (struct intr_frame *f UNUSED)
   uint32_t *esp = f->esp;
   //check_user_address(esp);
 
-  hex_dump(esp, esp, PHYS_BASE - esp, true);
+  hex_dump(esp, esp, 100, true);
 
   switch (*esp) {
     case SYS_HALT: // 0 arguement
