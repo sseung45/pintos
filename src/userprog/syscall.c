4ca2062f6b6e319ebb5aafab4efbb6fed77eb3ea
Junseo Kim <wnstodn123@postech.ac.kr>
2023-11-28 17:50:29 +0900

    memory mapped file 구현을 위한 syscall 선언

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -26,6 +26,10 @@ void close(int fd);
 void check_user_address(void *addr);
 void get_argument(int *esp, int *arg , int count);
 
+//memory mapped file
+mapid_t mmap (int fd, void *addr);
+void munmap (mapid_t);
+
 struct lock file_lock;
 
 struct file_info {
